57b7f6ea064716dad2b3073295647526bf047c05
JenniferEstelle <jen@jenniferestelle.com>
2017-05-05 21:04:05 -0400

    started app.js currentDate and attempting to establish start of week.

---
+++ app.js
@@ -0,0 +1,8 @@
+var trashday = (function () { 
+    var currentDate = moment().format('MMM Do YY');
+    var startOfWeek = moment().startOf('')
+
+    //get start of week
+    //get end of week
+
+ }());
