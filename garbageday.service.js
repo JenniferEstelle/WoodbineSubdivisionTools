e86bf1aacdfd1f40911fc6f23d0f6b3ee5001bfd
JenniferEstelle <jen@jenniferestelle.com>
2017-05-05 20:47:08 -0400

    test

---
+++ garbageday.service.js
@@ -0,0 +1,17 @@
+//left in progress
+
+GarbageDayModule.factory('CalculateGarbageDay', CalculateGarbageDay); 
+
+CalculateGarbageDay.$inject = [''];
+function CalculateGarbageDay () {
+  var service = this;
+
+  var holidays;
+  // factory function body that constructs shinyNewServiceInstance
+  service.getHolidays = function (holidays) {
+      holidays = {
+
+      }
+  }
+  return shinyNewServiceInstance;
+});
